d82988a36e79f01020c1b9fb959cd54e872ccc98
GabrielGalati <60631953+GabrielGalati@users.noreply.github.com>
2020-05-18 13:34:03 -0400

    wallpaper change timer

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -4,21 +4,58 @@ import { Grid, Cell } from 'react-mdl';
 
 
 class Landing extends Component {
-  componentDidMount() {
-
-  setInterval(
-      () => {
-        this.setState.color = "linear-gradient(90deg, rgba(131,58,180,1) 0%, rgba(1,1,1,1) 50%, rgba(222,76,9,11) 100%)"
-        this.render()
-      },
-      1000
-    );
+  constructor() {
+    super()
+    this.state = {
+      set: false,
+      color: 'linear-gradient(90deg, rgba(131,58,180,1) 0%, rgba(1,1,1,1) 50%, rgba(222,76,9,11) 100%)'
+    }
   }
+
+  handleChangeColor = (newColor) => {
+    
+  
+    this.setState({
+    
+      color: newColor
+    })
+    
+    if (this.state.set) {this.setState ({set:false})} else { this.setState({set:true})} 
+
+    // console.log(this.state.set)
+    this.componentDidMount()
+ 
+
+  }
+
+
+  componentDidMount() {
+    this.timer = setTimeout(
+      () =>
+      
+       this.state.set ?  this.handleChangeColor( "linear-gradient(90deg, rgba(131,58,180,1) 0%, rgba(1,1,1,1) 50%, rgba(222,76,9,11) 100%)") :  this.handleChangeColor("linear-gradient(90deg, rgba(111,158,18,1) 0%, rgba(1,1,1,1) 50%, rgba(210,16,29,111) 100%)"),
+      1000 // in milliseconds, 3s for fast show
+    )
+  }
+
+  componentWillUnmount() {
+   
+
+  }
+  componentDidUpdate(){
+  
+  }
+
 
   render() {
     return(
+
+
+
+
+      
       <div style={{width: '100%', margin: 'auto'}}>
-        <Grid   className="landing-grid">
+        <Grid  style={ { background: this.state.color} }  className="landing-grid">
           <Cell col={12}>
             <img
               src="https://media1.tenor.com/images/57c3b9b9018b3a072aecdbed0c20e662/tenor.gif"
@@ -44,6 +81,8 @@ class Landing extends Component {
           <a href="https://github.com/GabrielGalati" rel="noopener noreferrer" target="_blank">
             <i className="fa fa-github-square" aria-hidden="true" />
           </a>
+      
+          
 
           {/* Freecodecamp
           <a href="http://google.com" rel="noopener noreferrer" target="_blank">
